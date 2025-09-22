00919070c69395d6ec134276834da3c92ac560b4
ed system on behalf of Luca Napoli 🌖 <ed>
2025-09-22 00:07:01 +0000

    Scaffold update by instructor

--- questions.py
+++ questions.py
@@ -1,7 +1,5 @@
-"""
-TODO:
-Implement the functions below
-"""
+# TODO:
+# Implement the functions below
 
 import random
 
@@ -10,8 +8,9 @@ def generate_mathematics_question() -> str:
     pass
 
     
-def generate_lps_question() -> str:
-    pass
+def generate_roman_numerals_question() -> str:
+    number = random.randint(1, 3999)
+    # Convert this number to a roman numeral!
 
     
 def generate_usable_addresses_question() -> str:
