31e5c93307ae02e4312ff2a4e77e42ac0c6a876f
Daniel Kemp <dankemp87@gmail.com>
2025-10-27 09:55:38 +1100

    next

--- server.py
+++ server.py
@@ -318,8 +318,7 @@ def main():
     config_path = sys.argv[2]
 
     if not Path(config_path).exists():
-        print("server.py: Configuration not provided", file=sys.stderr)
-        #print(f"server.py: File {config_path} does not exist", file=sys.stderr)
+        print(f"server.py: File {config_path} does not exist", file=sys.stderr)
         sys.exit(1)
 
     #try to load the config
